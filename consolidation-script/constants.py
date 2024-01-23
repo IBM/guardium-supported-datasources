de2294246047ebdcb42b2be1ecc8aa8bd33d91fb
Ahmed Mujtaba <ahmedmujtaba@Ahmeds-MacBook-Air.local>
2024-01-23 08:00:42 -0500

    added consolidation script

---
+++ consolidation-script/constants.py
@@ -0,0 +1,7 @@
+# Constants
+FEATURE_KEY=["Network traffic","Local traffic","Encrypted traffic","Shared Memory","Kerberos","Blocking","Redaction","UID Chain","Compression","Query Rewrite","Instance Discovery","Protocol"]
+GUARDIUM_VERSIONS=["11","11.1","11.2","11.3","11.4"]
+
+
+CSV_FILE_PATH="/Users/ahmedmujtaba/Desktop/workspace/github.ibm.com/IBM/guardium-supported-datasources/supported_dbs_clean.csv"
+JSON_FILE_PATH="/Users/ahmedmujtaba/Desktop/workspace/github.ibm.com/IBM/guardium-supported-datasources/supported_dbs.json"
